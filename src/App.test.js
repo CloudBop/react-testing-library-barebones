3a775a972efd2d2f0a064a456911630c372154ae
Cloudbop <boppincolin@gmail.com>
2021-06-27 20:47:07 +0100

    cleanup, write a failing test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByRole('link',{name: /learn react/i});
-  expect(linkElement).toBeInTheDocument();
+test('write a test for the spec', () => {
+  
 });

f745a82f4c204c476f8d94cc33b25e7cca3ce32f
Cloudbop <boppincolin@gmail.com>
2021-06-27 23:35:57 +0100

    2 fixed tests, 1 broken

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
     <div className="App">
       <button 
         onClick={()=> !disabled && setBtnColor(newBtnColor)} 
-        style={{backgroundColor:btnColor}}
+        style={{backgroundColor: disabled? 'gray' : btnColor}}
         disabled={disabled}
       >
         Change to {newBtnColor}

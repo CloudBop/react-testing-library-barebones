90acbbf4c4fb31a32585de9ef5d3fd575a33245a
Cloudbop <boppincolin@gmail.com>
2021-06-28 00:53:17 +0100

    +configd prettier

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import './App.css';
 import { useState } from 'react';
 
-export function replaceCamelWithSpaces(colorName){
-  return colorName.replace(/\B([A-Z])\B/g,' $1')
+export function replaceCamelWithSpaces(colorName) {
+  return colorName.replace(/\B([A-Z])\B/g, ' $1')
 }
 
 function App() {
   const [btnColor, setBtnColor] = useState('MediumVioletRed');
   const [disabled, setDisabled] = useState(false);
-  const newBtnColor = btnColor==='MediumVioletRed' ? 'MidnightBlue' :'MediumVioletRed';
+  const newBtnColor = btnColor === 'MediumVioletRed' ? 'MidnightBlue' : 'MediumVioletRed';
   return (
     <div className="App">
-      <button 
-        onClick={()=> !disabled && setBtnColor(newBtnColor)} 
-        style={{backgroundColor: disabled? 'gray' : btnColor}}
+      <button
+        onClick={() => !disabled && setBtnColor(newBtnColor)}
+        style={{ backgroundColor: disabled ? 'gray' : btnColor }}
         disabled={disabled}
       >
         Change to {replaceCamelWithSpaces(newBtnColor)}
       </button>
-      <input 
-        onChange={(e)=>setDisabled(e.target.checked)} 
+      <input
+        onChange={(e) => setDisabled(e.target.checked)}
         type="checkbox"
         defaultChecked={disabled}
         aria-checked={disabled}

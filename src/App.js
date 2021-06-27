88f25c93eb07dd48159f8e21c453765fa80ab7ab
Cloudbop <boppincolin@gmail.com>
2021-06-27 21:19:26 +0100

    +PASS TEST

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [btnColor, setBtnColor] = useState('red');
+  const newBtnColor = btnColor==='red' ? 'blue' :'red';
   return (
     <div className="App">
-      <button style={{backgroundColor:'red'}}>Change to blue</button>
+      <button onClick={()=>setBtnColor(newBtnColor)} style={{backgroundColor:btnColor}}>Change to {newBtnColor}</button>
     </div>
   );
 }

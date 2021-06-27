e57827cd9295ffd9a87340972afd4032d745dee9
Cloudbop <boppincolin@gmail.com>
2021-06-27 23:03:34 +0100

    cleanup

--- src/App.js
+++ src/App.js
@@ -4,11 +4,25 @@ import { useState } from 'react';
 
 function App() {
   const [btnColor, setBtnColor] = useState('red');
+  const [disabled, setDisabled] = useState(false);
   const newBtnColor = btnColor==='red' ? 'blue' :'red';
   return (
     <div className="App">
-      <button onClick={()=>setBtnColor(newBtnColor)} style={{backgroundColor:btnColor}}>Change to {newBtnColor}</button>
-      <input type="checkbox" name="" id="" />
+      <button 
+        onClick={()=> !disabled && setBtnColor(newBtnColor)} 
+        style={{backgroundColor:btnColor}}
+        disabled={disabled}
+      >
+        Change to {newBtnColor}
+      </button>
+      <input 
+        onChange={(e)=>setDisabled(e.target.checked)} 
+        type="checkbox"
+        defaultChecked={disabled}
+        aria-checked={disabled}
+        name="enable-button-checkbox"
+        id="enable-button-checkbox"
+      />
     </div>
   );
 }

7d21bdf3cd5cba5abae8f4ae13fc72b4e820ec67
Cloudbop <boppincolin@gmail.com>
2021-06-27 20:59:49 +0100

    +FAILTEST

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,11 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('write a test for the spec', () => {
-  
-});
+test("init button has correct background-color",()=>{
+  // https://www.w3.org/TR/wai-aria/#role_definitions
+  // https://github.com/testing-library/jest-dom
+  render(<App/>);
+  // find an element with a role of button and text of 'Change to blue'
+  const colorButton = screen.getByRole('button', {name:"Change to blue"});
+  expect(colorButton).toHaveStyle({backgroundColor: 'red'});
+})
